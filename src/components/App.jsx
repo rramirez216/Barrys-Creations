5cb214c5e0d0324274eab44d604f7b8885935787
Ruben Ramirez <rramirez110911@gmail.com>
2023-02-02 21:52:15 -0800

    imports nav

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import GlobalStyles from './GlobalStyles'
+import Nav from './Header/Nav'
+function App() {
+  return (
+    <>
+      <Nav />
+      <GlobalStyles />
+      {/* <img src='https://via.placeholder.com/250.png/ ' alt='placeholder' /> */}
+    </>
+  )
+}
+
+export default App
