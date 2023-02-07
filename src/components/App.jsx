2e1f0541082823aaf7951edb982e13107429ee87
Ruben Ramirez <rramirez110911@gmail.com>
2023-02-06 22:35:06 -0800

    imports react router dom components and imports ui components from content directory and renders them

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,26 @@
 import styled from 'styled-components'
+import { Routes, Route, Outlet, Link } from 'react-router-dom'
+
 import GlobalStyles from './GlobalStyles'
-import Nav from './Header/Nav'
+import Header from './Header/Header'
+import Content from './Main/Content'
+import Footer from './Footer/Footer'
+import Gallery from './Main/Gallery/Gallery'
+import About from './Main/About/About'
+import TOS from './Main/TOS/TOS'
+
 function App() {
   return (
     <>
-      <Nav />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Content />}>
+          <Route index element={<Gallery />} />
+          <Route path='about' element={<About />} />
+          <Route path='tos' element={<TOS />} />
+        </Route>
+      </Routes>
+      <Footer />
       <GlobalStyles />
       {/* <img src='https://via.placeholder.com/250.png/ ' alt='placeholder' /> */}
     </>
